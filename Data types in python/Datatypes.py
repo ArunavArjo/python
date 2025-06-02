205066813406444057cafd0fd2aba5d8b77fdd8b
AunavArjo <arunavarjo@gmail.com>
2025-06-02 21:12:01 +0600

    Datatypes.py

---
+++ Data types in python/Datatypes.py
@@ -0,0 +1,11 @@
+name = "Rakib"
+print ("The datatype name is ",type (name))
+
+id = 40001
+print ("The datatype id is ",type (id))
+
+weight = 60.5
+print ("The datatype weight is ",type (weight))
+
+is_employee = True
+print ("The datatype is_employee is ",type (is_employee))
---
+++ Data types in python/typeCasting.py
@@ -0,0 +1,19 @@
+name = "Karim"
+age = 15
+weight = 50.6
+is_student = True
+print("Name:",name )
+print("Age:",age)
+print("Weight:",weight)
+print("Is_student:",is_student)
+
+print("-----After typecasting----")
+
+age = float(age)
+print("age:",age , "type:",type(age))
+
+weight = int(weight)
+print("weight:", weight, "type:",type(weight))
+
+is_student = int(is_student)
+print("is_student:", weight, "type:",type(is_student))
