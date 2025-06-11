48f4a2c751bed92fe3a08cd399facdc2224eea53
AunavArjo <arunavarjo@gmail.com>
2025-06-11 20:56:00 +0600

    BMI calculatar.py

---
+++ Python Operator II/BMI calcuatar.py
@@ -0,0 +1,18 @@
+height = float(input("Enter your height in meters:"))
+weight = float(input("Enter your weight in kilograms:"))
+
+BMI = weight/(height**2)
+print(f"Your BMI is{BMI:.2f}")
+
+if BMI < 18.5:
+    print("You are underweight")
+elif BMI < 25:
+    print("You are normal weight")
+elif BMI < 30:
+    print("You are overweight")
+elif BMI < 35:
+    print("You are severly overweight")
+elif BMI < 40:
+    print("You are obese")
+else:
+    print("You are extremly obese")
