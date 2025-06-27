7f7ae4ff3adf237bb89b56c548a4bb8e4f79d05d
AunavArjo <arunavarjo@gmail.com>
2025-06-27 23:14:42 +0600

    summer times.py

---
+++ acp/summer times.py
@@ -0,0 +1,10 @@
+num = int(input("Enter a number"))
+
+if num % 2 == 0:
+    print(num,"is even")
+else:
+  print(num,"is odd")
+if num  > 0:
+    print("The number is a psitive number")
+else:
+    print("The number is a negative number")
