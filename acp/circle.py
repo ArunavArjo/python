e73e11e066b4278005e437a1cc063ad3c6b03197
AunavArjo <arunavarjo@gmail.com>
2025-07-28 23:24:51 +0600

    circle.py

---
+++ acp/circle.py
@@ -0,0 +1,8 @@
+def find_circumference(r):
+    return 2 * 3.14 * r
+
+# Input from user
+radius = float(input("Enter radius: "))
+
+# Call the function and print result
+print("Circumference:", find_circumference(radius))
