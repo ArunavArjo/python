53b10af7b57871d058b35f8359bd1a52448f536e
AunavArjo <arunavarjo@gmail.com>
2025-08-13 20:40:18 +0600

    Sigma boy123.py

---
+++ Tuples/Flip-flop.py
@@ -0,0 +1,14 @@
+def flip_flop(tuple):
+    l = len(tuple )-1
+    f = 0
+    while(f<l):
+            if (tuple[l] != tuple[f]):
+                  return False
+            f +=1
+            l-+1
+            return True
+yu = (1,2,3,4,3,2,1,9)
+if(flip_flop(yu)):
+    print("The tuple is Flip_flop")
+else:
+      print("The tuple is not Flip_flop")
---
+++ Tuples/Tuples.py
@@ -0,0 +1,16 @@
+yu = ("Tuples",5,5.7,"hash")
+print(yu[3])
+
+yu = yu +(8,7)
+print(yu)
+
+yu1 = (5,6,7,5,6,7,5,6,7)
+print(yu1.count(5))
+
+Sliced = yu1[1:5]
+print(Sliced)
+
+Sliced = yu1[:5]
+print(Sliced)
+
+print(len(yu1))
