2b2f5bf3d2a11932c03655cacf5cac5c6eb44fdd
AunavArjo <arunavarjo@gmail.com>
2025-08-24 18:03:42 +0600

    Frequency.py

---
+++ acp/check the frequency.py
@@ -0,0 +1,13 @@
+codingal = {
+'python': 6,
+'Html': 9,
+'Scratch': 3,
+'Thunkable': 5,
+'cc+' : 9
+}
+res = 0
+k = 9
+for key in codingal:
+    if codingal[key] == k:
+        res +=1
+print("The frequencys are",k ,"=", res)
---
+++ get_rid_duplicale.py/tempCodeRunnerFile.py
@@ -0,0 +1,4 @@
+for key in fruits:
+    if fruits[key] == k:
+        res +=1
+print("The frequencys are",k ,"=", res)
