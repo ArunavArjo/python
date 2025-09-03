30068e5cb034ad7fe784719118a4c018d86dfa8d
AunavArjo <arunavarjo@gmail.com>
2025-09-03 23:48:38 +0600

    random password.py

--- acp/random password challenge.py
+++ acp/random password challenge.py
@@ -1,8 +1,8 @@
 import random
 def play():
-    number = random.randint(1,100)
+    number = random.randint(1000,100000)
     name = input("Enter your name:")
-    print(f"{name},Im thinking a number between 1 to 100")
+    print(f"{name},Im thinking a number between 1000 to 100000")
 
 
     Hint = "Even" if number%2 == 0 else "odd"
@@ -11,8 +11,8 @@ def play():
     for tries in range(6):
         guess = int(input("Take a guess:"))
 
-        if guess < 1 or guess > 100:
-            print("Take a number between 1 to 100")
+        if guess < 1000 or guess > 100000:
+            print("Take a number between 1000 to 100000")
             continue
         if guess < number:
             print("Too low")
