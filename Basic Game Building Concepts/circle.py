6aeab755d6346c619dc622bf4ea02706ef328da2
AunavArjo <arunavarjo@gmail.com>
2025-09-24 23:09:11 +0600

    color

---
+++ Basic Game Building Concepts/circle.py
@@ -0,0 +1,14 @@
+import pygame
+pygame.init()
+screen = pygame.display.set_mode((500,500))
+screen.fill == 'white'
+
+pygame.draw.circle(screen ,'green', (300,300),50)
+pygame.draw.circle(screen ,'blue' ,(100,100),50,3)
+pygame.display.update()
+running = True
+while  running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            done = False
+         
