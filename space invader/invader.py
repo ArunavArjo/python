22fc5a38a180439f25ba6b5eb873a2ce06c58eca
AunavArjo <arunavarjo@gmail.com>
2025-11-03 18:26:20 +0600

    loop

--- space invader/invader.py
+++ space invader/invader.py
@@ -25,7 +25,7 @@ screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
 
 # Background
-background = pygame.image.load('background.jpg')
+background = pygame.image.load('background.png')
 
 
 # Caption and Icon
