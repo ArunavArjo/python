d7ade1aa17b3cf981cb257a1a18ea4cffffe7a3f
AunavArjo <arunavarjo@gmail.com>
2025-11-04 19:01:25 +0600

    Exam are coming it might be last time of codding

--- GUI TIKINTER/Widgets for Starters!/Let’s Build a Text Editor.py
+++ GUI TIKINTER/Widgets for Starters!/Let’s Build a Text Editor.py
@@ -2,9 +2,9 @@ from tkinter import*
 from tkinter.filedialog import askopenfilename,asksaveasfilename
 window = Tk()
 window.title("Codingals Text editor")
-window.geometry("600x400")
+window.geometry("600x500")
 window.rowconfigure(0,minsize=800,weight=1)
-window.columnconfigure(0,minsize=800,weight=1)
+window.columnconfigure(1,minsize=800,weight=1)
 
 def open_file():
     """open a file for editing"""
@@ -31,17 +31,22 @@ def save_file():
     with open(filepath,"w") as output_file:
         text = txt_edit.get(1.0,END)
         output_file.write(text)
-    window.titlef("Codingals Text editor - {filepath}")
+    window.title(f"Codingals Text editor - {filepath}")
 
 txt_edit = Text(window)
-fr_buttons = Frame(window,relief=RAISED,bd=2)
-btn_open = Button(fr_buttons,text="open",command=open_file)
-btn_save = Button(fr_buttons,text="Save as",command=save_file)
 
-btn_open.grid(row=0,column=0,sticky="ew",padx=5,pady=5)
-btn_save.grid(row=0,column=0,sticky="ew",padx=5)
+fr_buttons = Frame(window, relief=RAISED, bd=2)
 
-fr_buttons.grid(row = 0,column=0,sticky="ns")
-txt_edit.grid(row = 0,column=1,sticky="nsew")
+btn_open = Button(fr_buttons, text="Open", command=open_file)
+
+btn_save = Button(fr_buttons, text="Save As", command=save_file)
+
+btn_open.grid(row=0, column=0, sticky="ew", padx=5, pady=5)
+
+btn_save.grid(row=1, column=0, sticky="ew", padx=5, pady=5)
+
+fr_buttons.grid(row=0, column=0, sticky="ns")
+
+txt_edit.grid(row=0, column=1, sticky="nsew")
 
 window.mainloop()
