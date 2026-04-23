2683397bed8e43b64027d60d8e628ee16afa8417
AunavArjo <arunavarjo@gmail.com>
2026-04-24 01:19:17 +0600

    goodwork

---
+++ Tuples/Tuple_product.py
@@ -0,0 +1,12 @@
+
+numbers_tuple = (2, 3, 4, 5)
+
+
+product = 1
+
+
+for num in numbers_tuple:
+    product *= num  
+
+print(f"The tuple is: {numbers_tuple}")
+print(f"The product of all numbers in the tuple is: {product}")
